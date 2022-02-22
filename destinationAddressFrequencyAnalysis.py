7ff4a58c5e6bc91371133dedd7e5e41d0ab2e323
Bryce Erdman <61336010+51Bryce@users.noreply.github.com>
2022-02-22 17:01:33 -0600

    Update destinationAddressFrequencyAnalysis.py
    By: Bryce Erdman
    This program counts how many times every source address appears in a pcap file.

--- destinationAddressFrequencyAnalysis.py
+++ destinationAddressFrequencyAnalysis.py
@@ -1,10 +1,12 @@
+#By: Bryce Erdman
+#This program counts how many times every source address appears in a pcap file.
 from doctest import script_from_examples
 from scapy.all import * #import scapy module
 from scapy.layers.inet import IP
 from collections import Counter
 
 A = []
-packets = rdpcap(r"C:\UWW\UndergraduateResearch\iotPrivacy\pcapFiles\Unfiltered\KardiaInstallation.pcapng")
+packets = rdpcap(r"C:\UWW\UndergraduateResearch\iotPrivacy\pcapFiles\Unfiltered\KardiaInstallation.pcapng") #put your own file path here
 for packet in packets: #for each packet in pcap file
     if(packet.haslayer(IP)): #if the packet has an IP layer
         A.append(packet[IP].src) #add the IP source address to array
