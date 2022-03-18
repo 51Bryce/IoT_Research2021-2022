ac92c1757ea16a88f316e1f7285ed033636ef4a0
Bryce Erdman <61336010+51Bryce@users.noreply.github.com>
2022-03-18 12:44:13 -0500

    Update destinationAddressFrequencyAnalysis.py

--- ScapyPrograms/destinationAddressFrequencyAnalysis.py
+++ ScapyPrograms/destinationAddressFrequencyAnalysis.py
@@ -1,35 +1,54 @@
 #By: Bryce Erdman
-#This program counts how many times every source address appears in a pcap file.
+#Program Features:
+    # - Counts how many times every destination address appears in a pcap file
+    # - Lists destination ports used for IP address
+    # - Gives description of every unique destination address (know where your data is going)
+#Problems:
+    # - Needs to precisely filter application data (there may be some external data from phone in pcap file)
+
 from doctest import script_from_examples
 from scapy.all import * #import scapy module
-from scapy.all import DNS #import scapy module
 from scapy.layers.inet import IP
 from scapy.layers.inet import UDP
 from collections import Counter
+import ipinfo
+from ipinfo.handler_utils import cache_key
 
 def analyze(filename):
     A = [] #IP destination addresses
-    B = [] #DNS information
-    C = [] #UDP destination port information
+    B = [] #UDP destination port information
     packets = rdpcap(filename)
     #packets = rdpcap(fileName)
     for packet in packets: #for each packet in pcap file
         if(packet.haslayer(IP) and packet.haslayer(UDP)): #if the packet has a UDP and IP layer
             if(packet[IP].src == '192.168.2.159'): #filtering only packets that are coming FROM our router
                 A.append(packet[IP].dst) #add the IP destination address to array
-                C.append(packet[IP].dst) #IP ADDRESSES IN THE C ARRAY ARE USING A DESTINATION PORT
-                C.append(packet[UDP].dport) #add the Destination port to array
+                B.append(packet[IP].dst) #IP ADDRESSES IN THE C ARRAY ARE USING A DESTINATION PORT
+                B.append(packet[UDP].dport) #add the Destination port to array
+
         elif(packet.haslayer(IP)): #if the packet has an IP layer
             if(packet[IP].src == '192.168.2.159'): #filtering only packets that are coming FROM our router
                 A.append(packet[IP].dst) #add the IP destination address to array
 
-        if(packet.haslayer(DNSRR) and packet.haslayer(IP)): #if the packet has an DNS layer
-            if(packet[IP].dst == '192.168.2.159'): #filtering only packets that are coming TO our router
-                B.append(packet.qd.qname) #add the IP destination address to array
-    #Array is complete
+    #Arrays are complete
     print("IPv4 Addresses:\n",Counter(A),"\n") #count how many times every destination address occurs in array
-    print("DNS Names:\n",Counter(B),"\n") #count how many times every dns name occurs in array
-    print("UDP Destination Ports:\n",Counter(C)) #count how many times every udp destination port occurs in array
+    print("UDP Destination Ports:\n",Counter(B),"\n") #count how many times every udp destination port occurs in array
+    print("IP Address Information:")
+
+    C = [] #Array of unique IP Addresses in pcap
+    for i in A:
+        if(i not in C): #Do not include our router
+            C.append(i)
+
+    #Retrieve and print information for all unique IP Addresses
+    access_token = 'b9d4962ac9d5d0'
+    handler = ipinfo.getHandler(access_token)
+    for i in range(len(C)):
+        ip_address = C[i]
+        details = handler.getDetails(ip_address)
+        print(details.all,"\n")
+        i+=1
+
     print('\n*********************************************************')
 
 def analyzeAll(): #put filepaths here for the pcap files you want to check
